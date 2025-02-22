ed343f34a09242d1089c283c605fc38636065753
Margaret Fero <MaggieFero@users.noreply.github.com>
2025-02-21 17:23:56 -0800

    Add Metadata

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Neat Systems Website",
+	url: "https://neatsystems.dev",
+	language: "en",
+	description: "Neat Systems is a new organization and will put more here soon.",
+	author: {
+		name: "Margaret Fero of Neat Systems",
+		email: "NeatSystemsWebsite@NeatSystems.dev",
+		url: "https://neatsystems.dev/"
+	}
+}
