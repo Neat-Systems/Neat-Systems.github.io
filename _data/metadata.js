9323731159059bceadffac434f976a0b0af20712
Margaret Fero <MaggieFero@users.noreply.github.com>
2025-08-21 16:32:55 -0700

    Update meta description

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ export default {
 	title: "Neat Systems",
 	url: "https://neatsystems.dev",
 	language: "en",
-	description: "Neat Systems is a new organization and will put more here soon.",
+	description: "Neat Systems is a consultancy helping startups from Seed to C evaluate and optimize their systems to avoid letting growth get in the way of your goals.",
 	author: {
 		name: "Margaret Fero of Neat Systems",
 		email: "NeatSystemsWebsite@NeatSystems.dev",
